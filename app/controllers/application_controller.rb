113954fc9d1a8e680c81b5d775a833dc3efade0c
christinezosche <christine.zosche@gmail.com>
2020-07-26 11:45:43 -0700

    Merging this commit will enable sessions in the Application controller, outline index, sign up and login methods in the controller, and assign a basic welcome view page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,30 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "intergalacticflights"
   end
 
   get "/" do
     erb :welcome
   end
+
+  get "/signup" do
+
+  end
+
+  post "/signup" do
+
+  end
+
+  get "/login" do
+
+  end
+
+  post "/login" do
+
+  end
+
+  helpers
 
 end
